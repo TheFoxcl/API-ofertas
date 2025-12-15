9d2166ae2029239e5df0cd086868cc2895ed3155
TheFoxcl <juan_camilobotero@hotmail.com>
2025-12-15 12:53:15 -0500

    vecel configuratio

--- src/utilities/logger.js
+++ src/utilities/logger.js
@@ -1,27 +1,38 @@
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, printf, colorize } = format;
 
+const isVercel = process.env.VERCEL === "1";
+
 const customFormat = printf(({ level, message, timestamp, stack }) => {
   return `${timestamp} [${level}] ${stack || message}`;
 });
+
+const loggerTransports = [
+  new transports.Console({
+    format: combine(colorize(), timestamp(), customFormat),
+  }),
+];
+
+if (!isVercel) {
+  loggerTransports.push(
+    new transports.File({
+      filename: "logs/error.log",
+      level: "error",
+    }),
+    new transports.File({
+      filename: "logs/combined.log",
+    })
+  );
+}
 
 const logger = createLogger({
   level: "info",
   format: combine(
     timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    format.errors({ stack: true }), // Guarda stack trace
+    format.errors({ stack: true }),
     customFormat
   ),
-  transports: [
-    new transports.Console({
-      format: combine(colorize(), timestamp(), customFormat),
-    }),
-    new transports.File({
-      filename: "logs/error.log",
-      level: "error",
-    }),
-    new transports.File({ filename: "logs/combined.log" }),
-  ],
+  transports: loggerTransports,
 });
 
 module.exports = logger;
