ca10620c8c13ba8247592d1b1ce56a3870a80e44
TheFoxcl <juan_camilobotero@hotmail.com>
2025-12-15 12:18:58 -0500

    vecel configuratio

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
-require("dotenv").config();
 const express = require("express");
-const setRoutes = require("./routes/index");
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// Set up routes
-setRoutes(app);
+require("./routes")(app);
 
-app.listen(3000, () => {
-  console.log("API listening on port 3000");
-});
-
-// Export the app
 module.exports = app;
